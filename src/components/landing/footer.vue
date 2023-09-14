96486be0d5896866ca684d03f344cc6a7f739b62
keshavpro1 <keshavprogrammer1@gmail.com>
2023-09-14 17:01:53 +0530

    Added landing page

---
+++ src/components/landing/footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="footer">
+    <div class="container">
+      <div class="footer-start">
+        <h5>© 2023 Copyright: <a href="$"> mikudStudy.com </a></h5>
+        <h6>Learning center focus</h6>
+        <div class="social">
+          <a href="#"><img src="images/fb.png"></a>
+          <a href="#"><img src="images/ig.png"></a>
+          <a href="#"><img src="images/link.png"></a>
+          <a href="#"><img src="images/wa.png"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+
+export default {
+  components: {},
+  data: () => {
+    return {}
+  },
+  methods: {},
+  computed: {
+
+
+  },
+}
+</script>
---
+++ src/views/landing/index.vue
@@ -0,0 +1,348 @@
+<template>
+  <landingHeader></landingHeader>
+  <div class="banner" style="background-image:url('/Landing/images/banner-back.png');">
+    <div class="container container-new">
+      <div class="row">
+        <div class="col-lg-5 col-md-5 col-sm-12">
+          <div class="banner-dtl">
+            <h2>Welcome to our Premier <span>Study Center in Raanana!</span></h2>
+            <h6>WE HAVE THE BEST APPROACH TO LEARN</h6>
+            <p>Unlock Your Child's Potential with Personalized Private Tutoring</p>
+            <div class="banner-btn">
+              <a href="javascript:;" class="btn-main">Read More</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-7 col-md-7 col-sm-12">
+          <div class="banner-left">
+            <img src="images/banner-img.png">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="about">
+    <div class="container">
+      <div class="about-main">
+        <div class="title-main">
+          <h4>About </h4>
+          <p>At Raanana, we understand the importance of a strong educational foundation. As parents, you want the best
+            for your child's academic journey, and that's where we come in. Our dedicated team of experienced educators is
+            committed to providing exceptional private tutoring services for students in grades 1 through 12.</p>
+        </div>
+        <form>
+          <h5>After registration (free of charge)</h5>
+          <p>the training will be sent to you immediately</p>
+          <div class="form-inp">
+            <label>Futll Name</label>
+            <input type="text" name="" placeholder="Name">
+          </div>
+          <div class="form-inp">
+            <label>Email Address</label>
+            <input type="email" name="" placeholder="Email">
+          </div>
+          <div class="form-inp">
+            <label>Phone</label>
+            <input type="tel" name="" placeholder="Phone">
+          </div>
+          <div class="about-btn">
+            <button class="btn-main">Reserve a place for me for training</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="choose" style="background-image:url('/Landing/images/choose-back.png');">
+    <div class="container">
+      <div class="title-main">
+        <h4>Why Choose Us?</h4>
+      </div>
+      <div class="choose-main">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="choose-box">
+              <div class="icon">
+                <img src="images/choose1.png">
+              </div>
+              <div class="choose-dtl">
+                <h6>Comprehensive Subjects</h6>
+                <p>Whether your child needs help with math, science, languages, or any other subject, we have you covered.
+                  Our tutors cover a wide range of subjects, making us a one-stop solution for all your child's academic
+                  needs.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="choose-box">
+              <div class="icon">
+                <img src="images/choose2.png">
+              </div>
+              <div class="choose-dtl">
+                <h6>Expert Educators</h6>
+                <p>Our team consists of highly qualified educators who are experts in their respective fields. They bring
+                  years of teaching experience and a passion for nurturing young minds, helping your child excel in their
+                  studies.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="choose-box">
+              <div class="icon">
+                <img src="images/choose3.png">
+              </div>
+              <div class="choose-dtl">
+                <h6>Personalized Approach</h6>
+                <p>Every child is unique, and we believe that their learning journey should reflect that. Our private
+                  tutoring sessions are tailored to your child's individual needs, learning style, and pace, ensuring they
+                  get the most out of every session.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="choose-box">
+              <div class="icon">
+                <img src="images/choose4.png">
+              </div>
+              <div class="choose-dtl">
+                <h6>Comprehensive Subjects</h6>
+                <p>Whether your child needs help with math, science, languages, or any other subject, we have you covered.
+                  Our tutors cover a wide range of subjects, making us a one-stop solution for all your child's academic
+                  needs.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="choose-box">
+              <div class="icon">
+                <img src="images/choose5.png">
+              </div>
+              <div class="choose-dtl">
+                <h6>Comprehensive Subjects</h6>
+                <p>Whether your child needs help with math, science, languages, or any other subject, we have you covered.
+                  Our tutors cover a wide range of subjects, making us a one-stop solution for all your child's academic
+                  needs.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="choose-box">
+              <div class="icon">
+                <img src="images/choose6.png">
+              </div>
+              <div class="choose-dtl">
+                <h6>Comprehensive Subjects</h6>
+                <p>Whether your child needs help with math, science, languages, or any other subject, we have you covered.
+                  Our tutors cover a wide range of subjects, making us a one-stop solution for all your child's academic
+                  needs.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="choose-box">
+              <div class="icon">
+                <img src="images/choose7.png">
+              </div>
+              <div class="choose-dtl">
+                <h6>Comprehensive Subjects</h6>
+                <p>Whether your child needs help with math, science, languages, or any other subject, we have you covered.
+                  Our tutors cover a wide range of subjects, making us a one-stop solution for all your child's academic
+                  needs.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="mehod" style="background-image:url('/Landing/images/method-bg.png')">
+    <div class="container container-main-new">
+      <div class="row">
+        <div class="method-start">
+          <div class="col-lg-5 col-md-5 col-sm-12">
+            <div class="method-img">
+              <img src="images/method-img.png">
+            </div>
+          </div>
+          <div class="col-lg-7 col-md-7 col-sm-12">
+            <div class="method-cont">
+              <div class="mth-title">
+                <h3>This is our method</h3>
+                <h6>!All The Ways to Sucess</h6>
+              </div>
+              <div class="mth-Boxs row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                  <div class="first-box blue">
+                    <h5>1</h5>
+                    <p>We envision a world where everyone has a role to play, no matter what age, in solving problems for
+                      the good of others.</p>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                  <div class="first-box green">
+                    <h5>2</h5>
+                    <p>We envision a world where everyone has a role to play, no matter what age, in solving problems for
+                      the good of others.</p>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                  <div class="first-box red">
+                    <h5>3</h5>
+                    <p>We envision a world where everyone has a role to play, no matter what age, in solving problems for
+                      the good of others.</p>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                  <div class="first-box yellow">
+                    <h5>4</h5>
+                    <p>We envision a world where everyone has a role to play, no matter what age, in solving problems for
+                      the good of others.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="invest" style="background-image:url('/Landing/images/invest-dot.png')">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="invest-left">
+            <img src="images/teaching.png">
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="invest-right">
+            <h3>Invest in Your Child's Future Today!</h3>
+            <p>Give your child the academic advantage they deserve. Join the [Your Study Center's Name] family and witness
+              the transformative power of personalized private tutoring. Contact us today to learn more about our
+              programs, tutors, and enrollment options. Together, let's pave the way for your child's success.</p>
+            <div class="invest-btn">
+              <a href="javascript:;" class="btn-main">Enroll Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="testi" style="background-image:url('/Landing/images/bg1a.png')">
+    <div class="container">
+      <div class="testi-title">
+        <h3>Students recommend us.</h3>
+      </div>
+      <div class="testi-slider">
+        <div class="test-sl">
+          <div class="tet-img">
+            <img src="images/usera.png">
+          </div>
+          <div class="tet-cont">
+            <p>I wanted to thank you for all the help before my match test! You really helped me, the test went well and I
+              was able to solve all the question</p>
+            <h3>Adiana Weinstock</h3>
+            <h5>11 student</h5>
+          </div>
+        </div>
+        <div class="test-sl">
+          <div class="tet-img">
+            <img src="images/usera.png">
+          </div>
+          <div class="tet-cont">
+            <p>I wanted to thank you for all the help before my match test! You really helped me, the test went well and I
+              was able to solve all the question</p>
+            <h3>Adiana Weinstock</h3>
+            <h5>11 student</h5>
+          </div>
+        </div>
+        <div class="test-sl">
+          <div class="tet-img">
+            <img src="images/usera.png">
+          </div>
+          <div class="tet-cont">
+            <p>I wanted to thank you for all the help before my match test! You really helped me, the test went well and I
+              was able to solve all the question</p>
+            <h3>Adiana Weinstock</h3>
+            <h5>11 student</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="teachers">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="teachers-left">
+            <h4>Who are our teachers?</h4>
+            <p>We hand-pick our teachers based on their proficiency in the field they are teaching and their amiable
+              personality that enables a strong personal connection to the student. This helps them internalize the
+              material yielding better performance and higher grades.</p>
+            <div class="teachers-btn">
+              <a href="javascript:;" class="btn-main">Join Us</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="teachers-right">
+            <img src="images/teacher.png">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="contact">
+    <div class="container">
+      <div class="contact-title">
+        <h4>Contact for more information!</h4>
+      </div>
+      <form>
+        <div class="form-inp">
+          <label>Futll Name</label>
+          <input type="text" name="" placeholder="Name">
+        </div>
+        <div class="form-inp">
+          <label>Email Address</label>
+          <input type="email" name="" placeholder="Email">
+        </div>
+        <div class="form-inp">
+          <label>Phone</label>
+          <input type="tel" name="" placeholder="Phone">
+        </div>
+        <div class="about-btn">
+          <button class="btn-main">Send</button>
+        </div>
+      </form>
+    </div>
+  </div>
+  <footer></footer>
+</template>
+<script>
+import landingHeader from '../../components/landing/header.vue';
+import footer from '../../components/landing/footer.vue';
+
+export default {
+  components: {
+    landingHeader,
+    footer,
+  },
+  data: () => {
+    return {
+      studentView: true,
+      showPlans: false,
+    }
+  },
+
+  async created() {
+    await this.$store.dispatch({ type: 'loadLessons' })
+  },
+  methods: {},
+  computed: {
+    user() {
+      return this.$store.getters.user
+    },
+  },
+}
+</script>
+
+
