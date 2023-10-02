0b9ed498aef7097c0d88352ebb7daa6840b6d5dc
keshavpro1 <keshavprogrammer1@gmail.com>
2023-10-02 11:19:03 +0530

    Api url changed on http service

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 const BASE_URL =
   process.env.NODE_ENV === "production"
     ? "https://mikudstudy.netlify.app/api/"
-    : "//localhost:3030/api/";
+    : "//92.205.167.60:3030/api/";
 
 var axios = Axios.create({
   withCredentials: true,
